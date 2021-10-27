b4436b6071f978b59aad32530f775ff486180ee0
Ginni Pinckert <gcpinckert@gmail.com>
2021-10-26 18:30:59 -0700

    Improve search function

--- book_reader.rb
+++ book_reader.rb
@@ -9,14 +9,34 @@ end
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map { |paragraph| "<p>#{paragraph}</p>" }.join
+    text.split("\n\n").map.with_index do |paragraph, index| 
+      "<p id='#{index}'>#{paragraph}</p>"
+    end
   end
 
   def search_chapters(search_terms)
     (1..@chapters.size).each_with_object({}) do |num, hash|
       text = File.read("data/chp#{num}.txt")
-      hash[num] = @chapters[num - 1] if text.include?(search_terms)
+      title = @chapters[num - 1]
+      if text.include?(search_terms)
+        hash[title] = { num: num, paragraphs: search_paragraphs(search_terms, text) }
+      end
     end
+  end
+
+  def search_paragraphs(search_terms, text)
+    paragraphs = {}
+    text.split("\n\n").each_with_index do |paragraph, index|
+      if paragraph.include?(search_terms)
+        paragraphs[index] = paragraph
+      end
+    end
+
+    paragraphs
+  end
+
+  def bold_search_terms(search_terms, text)
+    text.gsub(search_terms, "<b>#{search_terms}</b>")
   end
 end
 
